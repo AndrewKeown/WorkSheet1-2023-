4f1fbd80dc5cf5f76f99e62f4743458c32267fd3
Andrew Keown <S00238928@atu.ie>
2023-01-27 10:22:03 +0000

    Add project files.

---
+++ Worksheet1(2023)/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Worksheet1_2023_
+/* 
+
+* Name : Andrew Keown 
+
+* Desc : Question 1 
+
+* Write a method called PrintIntegersFor which uses a for loop to print all the integers 
+
+* between 40 and 60 in ascending order to the console 
+
+*/
+
+{
+
+    internal class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            PrintIntegersFor();
+
+        }
+
+
+
+
+
+        static void PrintIntegersFor()
+
+        {
+
+            for (int i = 40; i <= 60; i++)
+
+            {
+
+                Console.WriteLine(i);
+
+            }
+
+        }
+
+    }
+
+}
