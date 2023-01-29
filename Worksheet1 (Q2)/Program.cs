43a2a4d9f70bdee673d611970d495935624f6385
Andrew Keown <S00238928@atu.ie>
2023-01-29 22:11:40 +0000

    Question 1 and Question 2

---
+++ Worksheet1 (Q2)/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Worksheet1__Q2_
+{
+    /* Name : Andrew Keown
+     * Desc : Question 2 (A-B)
+     * 
+     * Write a method called CountOddNumbers, which reads in 5 int values from the console
+     * and returns the number of them which are odd
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
