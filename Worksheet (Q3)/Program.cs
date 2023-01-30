eed7f35cc53295d47f266855e986f720d28347df
Andrew Keown <S00238928@atu.ie>
2023-01-30 09:21:14 +0000

    Question 3

---
+++ Worksheet (Q3)/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Worksheet__Q3_
+{
+    /*
+     * Name : Andrew
+     * Date : 30/01/2023
+     * Desc : Question 3 (A-D)
+     * 
+     * Write a method called PrintTriangle
+     * to print (#) one at a time
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintTriangle();
+        }
+
+        static void PrintTriangle()
+        {
+            Console.WriteLine($"#");
+            Console.WriteLine($"##");
+            Console.WriteLine($"###");
+            Console.WriteLine($"###");
+            Console.WriteLine($"#####");
+
+
+        }
+
+
+
+
+    }
+}
